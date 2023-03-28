bf204ea25fcf811c0839f03d6acf84fad846303a
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-03-28 18:55:42 +0200

    primitive MovieDao implementation

---
+++ api/dao/MovieDao.class.php
@@ -0,0 +1,45 @@
+<?php
+class MovieDao {
+
+    private $connection;
+    private $hostname = "127.0.0.1:3307";
+    private $database = "filmsystem";
+    private $username = "root";
+    private $password = "edinbajric";
+
+    function __construct() {
+        try {
+            $this -> connection = new PDO("mysql:host=$this->hostname;dbname=$this->database;", $this -> username, $this -> password);
+            $this -> connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            echo "Error: " . $e -> getMessage();
+        }
+    }
+
+    function add($movie) {
+        $query = $this -> connection -> prepare("INSERT INTO movie (title, year, length, description, writer, actor, image) 
+        VALUES (:title, :year, :length, :description, :writer, :actor, :image)");
+        $query -> execute($movie);
+    }
+
+    function get() {
+        $query = $this -> connection -> prepare("SELECT * FROM movie");
+        $query -> execute();
+        return $query -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function update($attribute, $value, $id) {
+        $query = $this -> connection -> prepare("UPDATE movie SET $attribute = :value WHERE id = :id");
+        $query -> bindParam(':value', $value);
+        $query -> bindParam(':id', $id);
+        $query -> execute();
+    }
+
+    function delete($id) {
+        $query = $this -> connection -> prepare("DELETE FROM movie WHERE id = :id");
+        $query -> bindParam(':id', $id);
+        $query -> execute();
+    }
+}
+?>
