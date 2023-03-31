2c7b5b57a017c9c64026b24c42168f10923b3c24
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-03-31 16:57:01 +0200

    updated index.php with genre

--- api/index.php
+++ api/index.php
@@ -3,12 +3,15 @@ require dirname(__FILE__).'/../vendor/autoload.php';
 
 require dirname(__FILE__).'/services/MovieService.php';
 require dirname(__FILE__).'/services/DirectorService.php';
+require dirname(__FILE__).'/services/GenreService.php';
 
 require_once dirname(__FILE__).'/routes/MovieRoutes.php';
 require_once dirname(__FILE__).'/routes/DirectorRoutes.php';
+require_once dirname(__FILE__).'/routes/GenreRoutes.php';
 
 Flight::register('movieService', 'MovieService');
 Flight::register('directorService', 'DirectorService');
+Flight::register('genreService', 'GenreService');
 
 Flight::start();
 ?>
