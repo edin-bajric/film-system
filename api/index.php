ea72646be8ecde4036e19e98d5b85601d49d9fa0
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-03-31 16:34:01 +0200

    added "/" in front of require paths in index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-require dirname(__FILE__).'../vendor/autoload.php';
-require dirname(__FILE__).'services/MovieService.php';
-require_once dirname(__FILE__).'routes/MovieRoutes.php';
+require dirname(__FILE__).'/../vendor/autoload.php';
+require dirname(__FILE__).'/services/MovieService.php';
+require_once dirname(__FILE__).'/routes/MovieRoutes.php';
 
 Flight::register('movieService', 'MovieService');
 
