f0b9e03fe93b49c17bd7eedd287d32b102be6908
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-06-28 02:15:00 +0200

    added favorites and login and reg

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
   app.route({view: 'watchlist-page', load: 'watchlist.html'})
   app.route({view: 'favorites-movies-page', load: 'favorites-movies.html'})
   app.route({view: 'favorites-directors-page', load: 'favorites-directors.html'})
+  app.route({view: 'login-page', load: 'login.html'})
+  app.route({view: 'register-page', load: 'register.html'})
   app.route({view: 'single-movie-page', load: 'single-movie-page.html'})
   app.route({view: 'single-director-page', load: 'single-director-page.html'})  
   // run app
@@ -19,6 +21,43 @@ $(document).ready(function() {
   $(document).on('click', '#all-directors-link', function(e) {
     e.preventDefault();
     window.location.hash = 'favorites-directors-page';
+  });
+
+  $(document).on('click', '#all-movies-link', function(e) {
+    e.preventDefault();
+    window.location.hash = 'favorites-movies-page';
+  });
+
+  app.route({
+    view: 'login-page',
+    onCreate: function() {
+      $('.navbar').addClass('hidden');
+      $('.footer').addClass('hidden');
+    },
+    onReady: function() {
+      $('.navbar').addClass('hidden');
+      $('.footer').addClass('hidden');
+    }
+  });
+
+  app.route({
+    view: 'register-page',
+    onCreate: function() {
+      $('.navbar').addClass('hidden');
+      $('.footer').addClass('hidden');
+    },
+    onReady: function() {
+      $('.navbar').addClass('hidden');
+      $('.footer').addClass('hidden');
+    }
+  });
+
+  app.route({
+    view: 'homepage',
+    onReady: function() {
+      $('.navbar').removeClass('hidden');
+      $('.footer').removeClass('hidden');
+    }
   });
 
 });
