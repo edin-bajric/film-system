b22dbcae11133f0f41b65ca5fe6d17984417e60d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-07-01 01:00:15 +0200

    closing mobile menu fix

--- assets/js/button-function.js
+++ assets/js/button-function.js
@@ -6,6 +6,14 @@ hamburger.addEventListener("click", function () {
     mobile_menu.classList.toggle("is-open");
   });
 
+  const menuItems = document.querySelectorAll(".mobile-menu a");
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", function () {
+      mobile_menu.classList.remove("is-open");
+      hamburger.classList.remove("is-active");
+    });
+  });
+
 const back_to_top = document.querySelector(".back-to-top");
 
 back_to_top.addEventListener("click", function (event) {
