42472233181945f4b3738fd469a92024f87c2d9b
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-07-03 19:33:31 +0200

    cleanup

--- rest/dao/RatingDao.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/BaseDao.class.php';
-
-class RatingDao extends BaseDao {
-    public function __construct(){
-        parent::__construct("rating");
-    }
-}
-?>
--- rest/index.php
+++ rest/index.php
@@ -6,30 +6,27 @@ require dirname(__FILE__).'/services/MovieService.php';
 require dirname(__FILE__).'/services/DirectorService.php';
 require dirname(__FILE__).'/services/GenreService.php';
 require dirname(__FILE__).'/services/UserService.php';
-require dirname(__FILE__).'/services/RatingService.php';
 require dirname(__FILE__).'/services/WatchlistService.php';
 require dirname(__FILE__).'/services/FavoriteService.php';
-require dirname(__FILE__).'/services/CommentService.php';
+
 
 // routes
 require_once dirname(__FILE__).'/routes/MovieRoutes.php';
 require_once dirname(__FILE__).'/routes/DirectorRoutes.php';
 require_once dirname(__FILE__).'/routes/GenreRoutes.php';
 require_once dirname(__FILE__).'/routes/UserRoutes.php';
-require_once dirname(__FILE__).'/routes/RatingRoutes.php';
 require_once dirname(__FILE__).'/routes/WatchlistRoutes.php';
 require_once dirname(__FILE__).'/routes/FavoriteRoutes.php';
-require_once dirname(__FILE__).'/routes/CommentRoutes.php';
+
 
 // register
 Flight::register('movieService', 'MovieService');
 Flight::register('directorService', 'DirectorService');
 Flight::register('genreService', 'GenreService');
 Flight::register('userService', 'UserService');
-Flight::register('ratingService', 'RatingService');
 Flight::register('watchlistService', 'WatchlistService');
 Flight::register('favoriteService', 'FavoriteService');
-Flight::register('commentService', 'CommentService');
+
 
 Flight::start();
 ?>
