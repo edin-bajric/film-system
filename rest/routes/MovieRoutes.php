253b4da4a44e5370c3a864b2b687506c10ea9052
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-07-04 19:51:27 +0200

    swagger for movie routes

--- rest/routes/MovieRoutes.php
+++ rest/routes/MovieRoutes.php
@@ -52,9 +52,72 @@ Flight::route('DELETE /movie/@id', function($id){
     Flight::json(["message" => "Movie deleted successfully"]);
 });
 
+/**
+ * @OA\Get(
+ *     path="/full/movie",
+ *     summary="Get full movie details",
+ *     tags={"Movies"},
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation",
+ *         @OA\JsonContent(
+ *             type="array",
+ *             @OA\Items()
+ *         )
+ *     ),
+ *     security={
+ *         {"bearerAuth": {}}
+ *     }
+ * )
+ */
+
 Flight::route('GET /full/movie', function() {
     Flight::json(Flight::movieService()->get_full_movie());
 });
+
+/**
+ * @OA\Get(
+ *     path="/full/movie/{title}",
+ *     summary="Get full movie details by title",
+ *     tags={"Movies"},
+ *     @OA\Parameter(
+ *         name="title",
+ *         in="path",
+ *         description="The title of the movie",
+ *         required=true,
+ *         @OA\Schema(type="string", example="The Movie Title")
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="title",
+ *                 type="string",
+ *                 example="The Movie Title"
+ *             ),
+ *             @OA\Property(
+ *                 property="director",
+ *                 type="string",
+ *                 example="John Doe"
+ *             ),
+ *             @OA\Property(
+ *                 property="release_date",
+ *                 type="string",
+ *                 example="2022-01-01"
+ *             ),
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="Movie not found"
+ *     ),
+ *     security={
+ *         {"bearerAuth": {}}
+ *     }
+ * )
+ */
 
 Flight::route('GET /full/movie/@title', function($title) {
     Flight::json(Flight::movieService()->get_movie_by_title($title));
